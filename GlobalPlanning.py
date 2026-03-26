66696127ef79dc5c5d4f8e2e416593abe7f31316
Dima <pyatkov.dim@gmail.com>
2026-03-26 11:50:28 +0600

    MLP step 1

--- GlobalPlanning.py
+++ GlobalPlanning.py
@@ -1,1 +1,21 @@
-print("Hello world!")
+import pandas as pd
+import torch.nn as nn
+import torch.nn.functional as F
+
+df = pd.read_csv("C:\\Users\\User\\OneDrive\\Desktop\\УИРС\\SEM5\\filtered_robot_data.csv")
+
+class MLP:
+
+    def __init__(self, in1, out1, out2):
+
+        self.l1 = nn.Linear(in1, out1)
+        self.f1 = F.sigmoid()
+        self.l2 = nn.Linear(out1, out2)
+        self.f2 = F.sigmoid()
+    
+    def forward(self, vec):
+
+        
+
+
+        
