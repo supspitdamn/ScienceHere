c4af33b1d211c5e44bc90719c7873a0269dab6a3
supspitdamn <pyatkov.dim@gmail.com>
2026-04-09 10:48:09 +0600

    Разработано слияние двух нейронных сетей. Написана функция forward для получения выхода

---
+++ inputNN/neuro_physical_model/Train&Valid&Test.py
@@ -0,0 +1,345 @@
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optimizer
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, mean_absolute_percentage_error
+import numpy as np
+import os
+import optuna
+import datetime
+import json
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Обучение на {device}")
+
+df = pd.read_csv("C:\\Users\\User\\OneDrive\\Desktop\\УИРС\\SEM5\\filtered_robot_data.csv", encoding="cp1251", sep=";")
+
+df = pd.get_dummies(df[["m1setvel", "m2setvel", "m3setvel", "m1cur", "m2cur", "m3cur", "surf"]], columns = ["surf"], prefix = "type_")
+
+df.info()
+
+df["m1setvel"] = pd.to_numeric(df["m1setvel"], errors="coerce")
+df = df.dropna(subset=["m1vel", "m1setvel"])
+df = df[(df["m1vel"] > 0.01) & (df["m1setvel"] > 0.01)]
+
+train, temp = train_test_split(df, test_size = 0.2, random_state = 42, shuffle = True)
+val, test = train_test_split(temp, test_size = 0.5, random_state = 42, shuffle = True)
+
+features = ["m1setvel", "m2setvel", "m3setvel", "type__gray", "type__green", "type__table", "type__brown"]
+targets = ["m1cur", "m2cur", "m3cur"]
+
+scaler_x = StandardScaler()
+scaler_y = StandardScaler()
+
+x_train = torch.tensor(scaler_x.fit_transform(train[features]), dtype = torch.float32)
+y_train = torch.tensor(scaler_y.fit_transform(train[targets]), dtype = torch.float32)
+
+x_val = torch.tensor(scaler_x.transform(val[features]), dtype = torch.float32)
+y_val = torch.tensor(scaler_y.transform(val[targets]), dtype = torch.float32)
+
+x_test = torch.tensor(scaler_x.transform(test[features]), dtype = torch.float32)
+y_test = torch.tensor(scaler_y.transform(test[targets]), dtype = torch.float32)
+
+train_dataset = TensorDataset(x_train, y_train)
+
+val_dataset = TensorDataset(x_val, y_val)
+
+test_dataset = TensorDataset(x_test, y_test)
+
+class MLP(nn.Module):
+
+    def __init__(self, *args):
+
+        super().__init__()
+        self.struct = args
+        self.__layers = nn.ModuleList()
+
+        for i in range(len(args) - 1):
+
+            self.__layers.append(nn.Linear(args[i], args[i+1]))
+
+            if i < len(args) - 2:
+
+                self.__layers.append(nn.ReLU())
+    
+    def forward(self, vec: torch.Tensor):
+
+        for layer in self.__layers:
+
+            vec = layer(vec)
+
+        return vec
+
+    def teaching(self, epochs: int, op: torch.optim.Optimizer, train_loader: DataLoader, val_loader: DataLoader, save_path: str, model_state_dict: dict, verbose: bool = True, patience: int = 15, loss_func = None, lr: float = None, batch_size: int = None) -> float:
+
+        device = next(self.parameters()).device
+        best_val_loss = float("inf")
+
+        log_txt_path = os.path.join(save_path, "LOG.txt")
+        trigger = 0
+
+        epochs = range(epochs)
+
+        train_loss_res = []
+        val_loss_res = []
+
+        with open(log_txt_path, "w", encoding="utf-8") as log_file:
+
+            log_file.write(f"Обучаемая сеть: {type(self).__name__}, стукутра: {"->".join(map(str,self.struct))}, оптимизатор - {type(op).__name__}, шаг обучения - {lr}, функция потерь - {type(loss_func).__name__}, Эпох обучения: {epochs}, Размер батча: {batch_size}\n")
+
+            for _ in epochs:
+
+                train_epoch_loss = 0
+                val_epoch_loss = 0
+
+                self.train()
+
+                for x, y in train_loader:
+
+                    x, y = x.to(device), y.to(device)
+
+                    res = self(x)
+
+                    loss = loss_func(res, y)
+
+                    op.zero_grad()
+                    loss.backward()
+                    op.step()
+
+                    train_epoch_loss += loss.item()
+                
+                avg_loss = train_epoch_loss/len(train_loader)
+
+                train_loss_res.append(avg_loss)
+
+                self.eval()
+
+                for x, y in val_loader:
+
+                    x, y = x.to(device), y.to(device)
+
+                    with torch.no_grad():
+
+                        res = self(x)
+                        loss = loss_func(res, y)
+                        val_epoch_loss += loss.item()
+                
+                avg_val_loss = val_epoch_loss/len(val_loader)
+                val_loss_res.append(avg_val_loss)
+
+                if avg_val_loss < best_val_loss:
+                    best_val_loss = avg_val_loss
+                    trigger = 0
+
+                    log_file.write(f"->Модель сохранена на эпохе {_}\n")
+                    model_state_dict["model"] = self.state_dict()
+                    model_state_dict["optimizer"] = op.state_dict()
+                    model_state_dict["epoch"] = _
+                    file_path = os.path.join(save_path, f"MLPconfig.pth")
+                    torch.save(model_state_dict, file_path)
+                else:
+                    trigger += 1
+
+                
+                log_str = f"Эпоха: {_}, Лосс обучения: {avg_loss}, Лосс валидации: {avg_val_loss}\n"
+
+                if _ % 5 == 0:
+                    log_file.write(log_str)
+
+                if _ % 20 == 0 and verbose:
+
+                    print(f"Эпоха: {_}, Лосс обучения: {avg_loss}")
+                    print(f"Эпоха: {_}, Лосс валидации: {avg_val_loss}")
+                
+                if trigger >= patience:
+
+                    log_file.write(f"Остановка алг. Early Stopping\n")
+                    log_file.write(log_str)
+                    print(f"Останов. Эпоха : {_}, Лосс обучения: {avg_loss}")
+                    print(f"Останов. Эпоха : {_}, Лосс валидации: {avg_val_loss}")
+                    break
+            else:
+                log_file.write(f"Штатный останов на эпохе {_}\n")
+                log_file.write(f"Лосс обучения: {avg_loss},  Лосс валидации: {avg_val_loss}\n")
+                print(f"Запланированный конец обучения. Эпоха : {_}, Лосс обучения: {avg_loss}")
+                print(f"Запланированный конец обучения. Эпоха : {_}, Лосс валидации: {avg_val_loss}")
+            
+        plt.plot(range(len(train_loss_res)), train_loss_res, color="blue", label = "Обучение")
+        plt.plot(range(len(val_loss_res)), val_loss_res, color = "red", label = "Валидация")
+
+        plt.legend()
+        plt.title(f"Функция потерь {type(self).__name__}_{'-'.join(map(str, self.struct))}_{type(op).__name__}_{lr}_{type(loss_func).__name__}")
+        plt.xlabel("Эпохи обучения")
+        plt.ylabel(f"Лосс {type(loss_func).__name__}")
+        plt.grid(visible=True)
+
+        png_path = os.path.join(save_path, f"Loss_MLP.png")
+        plt.savefig(png_path, dpi = 300, bbox_inches = "tight")
+
+        if verbose:
+            plt.show()
+        
+        plt.close()
+        
+        return min(val_loss_res)
+    
+    def evaluate(self, data_loader: DataLoader, scaler_y : StandardScaler, save_path: str, name: str, device: str = "cpu") -> dict:
+
+        all_pred = []
+        all_true = []
+        self.eval()
+        for x, y in data_loader:
+
+            x, y = x.to(device), y.to(device)
+
+            with torch.no_grad():
+
+                predict = self.forward(x).detach().cpu().numpy()
+                y = y.detach().cpu().numpy()
+
+                predict = (scaler_y.inverse_transform(predict))
+                true_value = (scaler_y.inverse_transform(y))
+
+                all_true.append(true_value)
+                all_pred.append(predict)
+
+        all_pred = np.vstack(all_pred)
+        all_true = np.vstack(all_true)
+
+        with open(os.path.join(save_path, "LOG.txt"), "w", encoding="utf-8") as log_txt:
+
+            mse = mean_squared_error(all_pred, all_true, multioutput="raw_values")
+            mae = mean_absolute_error(all_pred, all_true, multioutput="raw_values")
+            mape = mean_absolute_percentage_error(all_pred, all_true, multioutput="raw_values")
+            r2 = r2_score(all_true, all_pred, multioutput="raw_values")
+            
+            if name == "test":
+                log_txt.write(20*"-"+"\n")
+                log_txt.write("Результаты для тестовой выборки:\n")
+                log_txt.write(f"Абсолютная ошибка (M1, M2, M3): {'  '.join(map(str, np.round(mae, 4)))}\n")
+                log_txt.write(f"Относительная ошибка (M1, M2, M3): {'  '.join(map(str, np.round(mape * 100, 4)))}\n")
+
+        return {"MSE": tuple(mse), "MAE" : tuple(mae), "MAPE" : tuple(mape), "R2" : tuple(r2)}
+    
+    @staticmethod
+
+    def objective(trial, train_dataset, val_dataset, root_path, device = "cpu")->float:
+
+        lr = trial.suggest_float("lr", 1e-4, 1e-2)
+        num_layers = trial.suggest_int("num_layers", 1, 5)
+        hidden_size = trial.suggest_categorical("hidden_size", [16, 32, 64, 128])
+        b_size = trial.suggest_categorical("batch_size", [16, 32, 64, 128])
+        weight_decay = trial.suggest_float("weight_decay", 0, 1e-3)
+
+        layers_struct = [5] + [hidden_size]*num_layers + [1]
+
+        trial_model = MLP(*layers_struct).to(device=device)
+
+        trial_op = torch.optim.Adam(trial_model.parameters(), lr=lr, weight_decay=weight_decay)
+        trial_loss = nn.MSELoss()
+
+        v_load = DataLoader(val_dataset, batch_size=b_size, shuffle=False)
+        t_load = DataLoader(train_dataset, batch_size=b_size, shuffle=True)
+
+        save_path = os.path.join(root_path, f"MLP_{trial.number}_{"-".join(map(str, trial_model.struct))}_{type(trial_op).__name__}_{lr}_{type(trial_loss).__name__}_Batch_{b_size}")
+        os.makedirs(save_path, exist_ok = True)
+
+        best_val_loss = trial_model.teaching(
+            epochs=100, 
+            train_loader=t_load, 
+            val_loader=v_load, 
+            model_state_dict={},
+            op=trial_op,
+            loss_func=trial_loss,
+            lr=lr,
+            batch_size=b_size,
+            save_path=save_path,
+            verbose=False,
+            patience=10
+        )
+
+        return best_val_loss
+
+class NPM(nn.Module):
+
+    def __init__(self, seq_neur: list) -> None:
+        super().__init__()
+        self.stage_1 = nn.ModuleList(seq_neur[0])
+        self.stage_2 = seq_neur[1]
+    
+    def forward(self, vec: torch.Tensor):
+        surfs = vec[:, 3:7]
+
+        in1 = torch.cat((vec[:, 0:1], surfs), dim = 1)
+        in2 = torch.cat((vec[:, 1:2], surfs), dim = 1)
+        in3 = torch.cat((vec[:, 2:3], surfs), dim = 1)
+
+        v1_real = self.stage_1[0](in1)
+        v2_real = self.stage_1[1](in2)
+        v3_real = self.stage_1[2](in3)
+
+        real_velocities = torch.cat((v1_real, v2_real, v3_real), dim = 1)
+
+        in_st2 = torch.cat((real_velocities, surfs), dim = 1)
+
+        return self.stage_2(in_st2)
+
+best_par_sv_v = torch.load("SetVelocity_To_RealVelocity\MLP_study_20260408_230054\MLP_33_5-64-64-1_Adam_0.00782255740687542_MSELoss_Batch_128\MLPconfig.pth")
+best_par_v_c = torch.load("RealVelocity_To_Current\MLP_study_20260331_211138\MLP_7-96-96-96-3_Adam_0.000746242532647897_MSELoss_Batch_32\MLPconfig.pth")
+
+mlp_sv1_v1 = MLP(5, 64, 64, 1).load_state_dict(best_par_sv_v)
+mlp_sv2_v2 = MLP(5, 64, 64, 1).load_state_dict(best_par_sv_v)
+mlp_sv3_v3 = MLP(5, 64, 64, 1).load_state_dict(best_par_sv_v)
+mlp_vs_cs = MLP(7, 96, 96, 96, 3).load_state_dict(best_par_v_c)
+
+npm = NPM([[mlp_sv1_v1, mlp_sv2_v2, mlp_sv3_v3],mlp_vs_cs])
+
+timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+root_path = f".//SetVelocity_to_RealVelocity//MLP_study_{timestamp}"
+os.makedirs(root_path, exist_ok = True)
+
+study = optuna.create_study(direction="minimize")
+study.optimize(lambda trial: MLP.objective(trial, train_dataset, val_dataset, root_path, device), n_trials=40)
+
+best_trial = study.best_trial
+
+result = {"Best trial number" : best_trial.number,
+          "Best loss" : best_trial.value,
+          "Best parameters" : best_trial.params,
+          }
+
+train_loader = DataLoader(train_dataset, batch_size = best_trial.params["batch_size"], shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size = best_trial.params["batch_size"], shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size = best_trial.params["batch_size"], shuffle = False)
+
+with open(os.path.join(root_path, "optuna_results.json"), "w") as res:
+    json.dump(result, res, indent=4, ensure_ascii=False)
+
+data = {"train" : train_loader,
+         "val" : val_loader,
+           "test" : test_loader}
+
+best_struct = [5] + [best_trial.params["hidden_size"]]*best_trial.params["num_layers"] + [1]
+model = MLP(*best_struct).to(device)
+
+best_trial_folder = f"MLP_{best_trial.number}_{'-'.join(map(str, best_struct))}_Adam_{best_trial.params['lr']}_MSELoss_Batch_{best_trial.params["batch_size"]}"
+best_weight_path = os.path.join(root_path, best_trial_folder, "MLPconfig.pth")
+checkpoint = torch.load(best_weight_path, map_location=device)
+model.load_state_dict(checkpoint["model"])
+
+for key, value in data.items():
+
+    res = model.evaluate(value, scaler_y=scaler_y, name=key, save_path=root_path, device=device)
+
+    metrics_df = pd.DataFrame(res, index=["Двигатель 1", "Двигатель 2", "Двигатель 3"]).T
+
+    metrics_df.loc["MAPE, %"] = metrics_df.loc["MAPE"]*100
+    metrics_df.drop("MAPE", inplace=True)
+
+    table_path = os.path.join(root_path, f"FINAL_MLP_metrics_{key}.csv")
+    metrics_df.to_csv(table_path, index_label="Метрики", encoding="utf-8-sig")
