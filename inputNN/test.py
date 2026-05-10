4be95cf208b5d0565c675a7efdb04d3a4da127d8
supspitdamn <pyatkov.dim@gmail.com>
2026-05-10 14:21:34 +0600

    Для теста

--- inputNN/test.py
+++ inputNN/test.py
@@ -1,4 +1,17 @@
-import torch
+import pandas as pd
+import matplotlib.pyplot as plt
 
-print(torch.__version__)
-print(torch.cuda.is_available())
+df = pd.read_csv("C:\\Users\\User\\OneDrive\\Desktop\\УИРС\\SEM5\\filtered_robot_data.csv", encoding="cp1251", sep=";")
+
+surf_cols = ["surf"]
+vel_cols = ["m1vel", "m2vel", "m3vel"]
+setvel_cols = ["m1setvel", "m2setvel", "m3setvel"]
+
+print(f"{df[df["w1linslip"].abs() < 0.01].shape[0]} из {df.shape[0]}")
+print(max(df["w1slip"]))
+
+df["w1slip"].hist(bins=50)  # bins — это количество столбиков
+plt.title("Распределение проскальзывания w1slip")
+plt.xlabel("Значение")
+plt.ylabel("Частота")
+plt.show()
